185ddb1e735818e2d24e0264db53658ab8145669
Michelle Oh <michelleoh@6ef169c3-1b9c-4da9-8762-f41b87d2dfff.attlocal.net>
2025-03-26 23:32:20 -0400

    Added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import './contact.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import bluegalaxy from '../images/bluegalaxy.jpg';
+import { useState } from 'react';
 
 export default function Contact() {
+    // Reference --> https://web3forms.com/#start
+    // Note: free plan is limited to 250 submissions per month + basic features
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "3204f463-5382-4ebe-b95e-a6ee16c07ca6");  // public access key, alias to your email
+    
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+    
+        const data = await response.json();
+    
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
 
     return (
-        <div style={{ backgroundImage: `url(${bluegalaxy})`, backgroundSize: 'cover', width: '100vw', height: '100vh' }}>
-            <div className="contact">
-                <h1>Contact Me</h1>
-                <ul class="fa-ul">
-                    <li><a href="mailto:ohmotrl@gmail.com" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faEnvelope} />
-                         Email</a></li>
-                </ul>
-            </div>
+        <div style={{ backgroundImage: `url(${bluegalaxy})`, backgroundSize: 'cover', width: '100vw' }}> 
+            <h1>Contact Me</h1>
+
+            <section className="contact">
+                <form onSubmit={onSubmit} >
+                    <h2>Contact Form</h2>
+                    <div className="input-box">
+                        <label>Name: </label>
+                        <input type="text" className="field" placeholder='Enter your name' name="name" required />
+                    </div>
+                    <div className="input-box">
+                        <label>Email: </label>
+                        <input type="email" className="field" placeholder='Your email' name="email" required />
+                    </div>
+                    <div className="input-box">
+                        <label>Message: </label>
+                        <textarea className="field mess" placeholder='Type your message' name="message" required></textarea>
+                    </div>
+                    <button type="submit">Send message</button>
+                <span>{result}</span>
+                </form>
+            </section>
+                
                 <div className="photo-credit"><p>Photo by <a href="https://unsplash.com/@nicolescapturedmoments?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nicole Avagliano</a> on <a href="https://unsplash.com/photos/blue-and-black-galaxy-illustration-StCrWOWx2E8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
             </p></div>
         </div>
